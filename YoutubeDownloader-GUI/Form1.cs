c14a79837af75424430634c0169b70ce8f76d617
Cosmin Popescu <cosmin.popescu93@gmail.com>
2017-09-09 19:29:05 +0300

    Make download method async
    Progress of download is shown in a progress bar

--- YoutubeDownloader-GUI/Form1.cs
+++ YoutubeDownloader-GUI/Form1.cs
@@ -23,36 +23,40 @@ namespace YoutubeDownloader_GUI
 
         private  void button1_Click(object sender, EventArgs e)
         {
+            backgroundWorker1.RunWorkerAsync();           
+        }
 
+        //this method download the video in background. Also makes the GUI responsive
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
             try
             {
                 string link = textBox1.Text;
                 IEnumerable<VideoInfo> videoInfo = DownloadUrlResolver.GetDownloadUrls(link);
-                VideoInfo video = videoInfo.FirstOrDefault();
-                    //.First(info => info.VideoType == VideoType.Flash && info.Resolution == 480);
+                VideoInfo video = videoInfo.FirstOrDefault();//we take the first video from that url.
+                //.First(info => info.VideoType == VideoType.Flash && info.Resolution == 480);
 
                 if (video.RequiresDecryption)
                 {
                     DownloadUrlResolver.DecryptDownloadUrl(video);
                 }
                 var videoDownloader = new VideoDownloader(video, Path.Combine(@"C:\Users\cosmi\Desktop\youtube downlaoder gui test", video.Title + video.VideoExtension));
+
+                //report the progress for progressbar1
+                videoDownloader.DownloadProgressChanged += (y, x) => backgroundWorker1.ReportProgress((int)x.ProgressPercentage); 
                 videoDownloader.Execute();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
-
-        }
-
-        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
-        {
-
         }
 
         private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-
+            //show the progress
+            progressBar1.Value = e.ProgressPercentage;
+            this.Text = e.ProgressPercentage.ToString();
         }
     }
 }
